b806b5d101a11c045905302f090aae42c4076e42
Steven Burgess <steven@ainchor.co>
2024-07-20 23:14:44 -0400

    Initial Commit

---
+++ scripts/LeftMonitorUI.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func show_monitor():
+	print("asdf")
+	self.visible = true
